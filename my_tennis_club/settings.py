ef78bbf593de584d1fafe63302ddba1882008a92
Eb1n-Babu <ebin.babu.in@icloud.com>
2025-07-16 16:47:36 +0530

    secured the secret key

--- my_tennis_club/settings.py
+++ my_tennis_club/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0%odmsjx96!h@qg8)&0s9m!ts57vsmf*h018!)q*o+x%wi0gt+'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -86,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'python',
         'USER': 'root',
-        'PASSWORD': '@Ebinbabu2209',
+        'PASSWORD': '',
         'HOST': 'localhost',  # Change if using a remote server
         'PORT': '3306',       # Default MySQL port
     }
